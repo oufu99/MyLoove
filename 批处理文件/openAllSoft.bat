3dabc65520c2f572903fa0d1323ad7621d6aec22
打野的小妞 <oufu99@126.com>
2019-02-23 00:31:54 +0800

    初次添加

---
+++ 批处理文件/openAllSoft.bat
@@ -0,0 +1,6 @@
+@echo off
+start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
+start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"
+start "" "E:\ZPCode\SERP\SERP3.0\SERP3.0.sln"
+start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
+exit
