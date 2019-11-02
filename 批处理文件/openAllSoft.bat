35f2f4854ff5a1cc642438e2172822006512eec1
打野的小妞 <oufu99@gmail.com>
2019-11-02 12:25:38 +0800

    优化结构

--- 批处理文件/openAllSoft.bat
+++ 批处理文件/openAllSoft.bat
@@ -1,6 +1,12 @@
 @echo off
-start "" "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
-start "" "C:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"
-start "" "E:\ZPCode\SERP\SERP3.0\SERP3.0.sln"
-start "" "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe"
+:: 获取管理员权限
+cd /d %~dp0
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+
+:: 两个::是注释
+start "" "D:\MyLoove\VimD\userPlugins\InitProgram.ahk"
+start "" "D:\MyLoove\VimD\vimd.exe"
+:: vid有点问题先关闭再打开
+taskkill /f /im vimd.exe
+start "" "D:\MyLoove\VimD\vimd.exe"
 exit
