092076c7b0cc54d81ea1c4a331652b1328dfcd14
打野的小妞 <oufu99@gmail.com>
2019-11-07 08:52:09 +0800

    VimD换一下位置

--- 批处理文件/openAllSoft.bat
+++ 批处理文件/openAllSoft.bat
@@ -4,9 +4,9 @@ cd /d %~dp0
 %1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
 
 :: 两个::是注释
-start "" "D:\MyLoove\VimD\userPlugins\InitProgram.ahk"
-start "" "D:\MyLoove\VimD\vimd.exe"
+start "" "D:\Common\VimD\userPlugins\InitProgram.ahk"
+start "" "D:\Common\VimD\vimd.exe"
 :: vid有点问题先关闭再打开
 taskkill /f /im vimd.exe
-start "" "D:\MyLoove\VimD\vimd.exe"
+start "" "D:\Common\VimD\vimd.exe"
 exit
